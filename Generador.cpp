5de8e90c25ec370234927c2f84176691e50f6729
Rodrigo Alexander Becerra Lucano <51519114+RAlexander777@users.noreply.github.com>
2019-07-08 23:15:41 -0500

    Add files via upload

--- Generador.cpp
+++ Generador.cpp
@@ -4,18 +4,61 @@
 #include <stdlib.h>
 #include <time.h>
 #include <fstream>
+#include <windows.h>
 
-#define Dim 161  /// 80 * 2 + 1 = 161 : 80 representa el ancho de la pantalla de consola
-#define CELL 6400  /// 80 * 80 = 6400
-#define muro 1
-#define camino 0
+#define Dim 161  // 80 * 2 + 1 = 161 : 80 representa el ancho de la pantalla de consola
+#define Celdas 6400  // 80 * 80 = 6400
+#define muro 1 // La pared es true
+#define camino 0 // El camino es false
 
 using namespace std;
 
+void gotoxy(int x,int y);
+void Finesi();
+void Menu();
+void Puntos();
+
 void plantilla(int maze[Dim][Dim]);
-void generador(int indeks, int maze[Dim][Dim], int x_retorno[CELL], int y_retorno[CELL], int x, int y, int n, int visitado);
+void generador(int indeks, int maze[Dim][Dim], int x_retorno[Celdas], int y_retorno[Celdas], int x, int y, int n, int visitado);
 void mostrar(int maze[Dim][Dim], int dimension);
 int is_closed(int maze[Dim][Dim], int x, int y);
+
+void gotoxy(int x,int y)
+{  
+      HANDLE hcon;  
+      hcon = GetStdHandle(STD_OUTPUT_HANDLE);  
+      COORD dwPos;  
+      dwPos.X = x;  
+      dwPos.Y= y;  
+      SetConsoleCursorPosition(hcon,dwPos); 
+}
+
+void Puntos()
+{
+	for(int i=0; i<3; i++)
+	{
+		cout<<".";
+		Sleep(1000);
+	}
+}
+
+void Finesi()
+{
+	gotoxy(37,11);
+	cout<<"F"; Sleep(100);
+	cout<<"I"; Sleep(100);
+	cout<<"N"; Sleep(100);
+	cout<<"E"; Sleep(100);
+	cout<<"S"; Sleep(100);
+	cout<<"I"; Sleep(1000);
+	
+	gotoxy(2,21);
+	cout<<"Creado Por:";
+	gotoxy(2,22);
+	cout<<"Rodrigo Alexander Becerra Lucano";
+	Sleep(4000);
+	system("cls");
+}
 
 void plantilla(int maze[Dim][Dim])
 {
@@ -32,7 +75,7 @@ void plantilla(int maze[Dim][Dim])
      
 }
 
-void generador(int indeks, int maze[Dim][Dim], int x_retorno[CELL], int y_retorno[CELL], int x, int y, int n, int visitado)
+void generador(int indeks, int maze[Dim][Dim], int x_retorno[Celdas], int y_retorno[Celdas], int x, int y, int n, int visitado)
 {
     if(visitado< n*n)
     {
@@ -83,7 +126,6 @@ void generador(int indeks, int maze[Dim][Dim], int x_retorno[CELL], int y_retorn
 
         if(neighbour_valid == -1)
         {
-            // backtrack
             x_sig = x_retorno[indeks];
             y_sig = y_retorno[indeks];
             indeks--;
@@ -156,14 +198,16 @@ int is_closed(int maze[Dim][Dim], int x, int y)
     return 0;
 }
 
-int main()
+void Menu()
 {
 	
 	int dimension;
+	char op;
 	
-	system("color 0A");
+	gotoxy(22,9);
+	cout<<"Ingresa la dimension del laberinto: ";
 	
-	cout<<"\n Ingresa la dimension del laberinto: ";
+	gotoxy(39,11);
 	cin>>dimension;
 	
 	system("cls");
@@ -172,17 +216,59 @@ int main()
     int size= dimension / 2;
     int indeks= 0;
     int maze[Dim][Dim];
-    int x_retorno[CELL];
-    int y_retorno[CELL];
+    int x_retorno[Celdas];
+    int y_retorno[Celdas];
 
     plantilla(maze);
 
     x_retorno[indeks]= 1;
     y_retorno[indeks]= 1;
+    
+    gotoxy(35,11);
+    cout<<"Generando";
+    Puntos();
+    
+    system("cls");
 
     generador(indeks, maze, x_retorno, y_retorno, 1, 1, size, 1);
     maze[0][1]= maze[size * 2][size * 2 - 1]= ' ';
     mostrar(maze, size);
 
+	if(dimension <= 40)
+	{
+	    gotoxy(dimension+5,3);
+	    cout<<"Generar otro Laberinto? (Y/N)";
+	    gotoxy(dimension+18,5);
+	    cin>>op;
+	}
+	
+	else if(dimension > 40)
+	{
+		gotoxy(1,dimension+5);
+	    cout<<"Generar otro Laberinto? (Y/N): ";
+	    gotoxy(35,dimension+5);
+	    cin>>op;
+	}
+    
+    if(op=='Y')
+    {
+    	system("cls");
+		Menu();
+	}
+	
+	else
+	{
+		system("cls");
+	}
+}
+
+int main()
+{
+	system("color 0A");
+	
+	Finesi();
+	
+	Menu();
+ 
     return 0;
 }
